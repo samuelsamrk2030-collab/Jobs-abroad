bb9711e7e6cfa562ac9e4dbc93b2664beb2bd475
samuelsamrk2030-collab <samuelsamrk2030@gmail.com>
2026-04-08 09:55:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -19,17 +19,50 @@ app.post("/apply", (req, res) => {
   });
 });
 
-app.post("/pay", (req, res) => {
+app.post("/pay", async (req, res) => {
   const { phone, amount } = req.body;
-  console.log("Payment Request:", phone, amount);
 
-  res.json({
-    success: true,
-    message: "Payment initiated (mock)"
-  });
+  const auth = Buffer.from(
+  process.env.PAYHERO_USERNAME + ":" + process.env.PAYHERO_PASSWORD
+).toString("base64");
+
+  try {
+    const response = await fetch("https://backend.payhero.co.ke/api/v2/payments", {
+      method: "POST",
+      headers: {
+        "Authorization": `Basic ${auth}`,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        channel_id: process.env.PAYHERO_CHANNEL_ID,
+        phone_number: phone,
+        amount: amount,
+        account_reference: "Elite Global Careers",
+        transaction_desc: "Job Application Payment",
+        callback_url: "https://jobs-abroad-r8k0.onrender.com/callback"
+      })
+    });
+
+    const data = await response.json();
+    res.json(data);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Payment failed" });
+  }
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/callback", (req, res) => {
+  console.log("PAYMENT CALLBACK:", req.body);
+
+  if (req.body.status === "SUCCESS") {
+    console.log("✅ Payment Successful");
+  } else {
+    console.log("❌ Payment Failed");
+  }
+
+  res.sendStatus(200);
+});
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
