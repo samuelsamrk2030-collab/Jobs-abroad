8489d924757318cbd00c2404c970bf126a4ffde6
samuelsamrk2030-collab <samuelsamrk2030@gmail.com>
2026-04-08 10:39:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -63,6 +63,8 @@ app.post("/callback", (req, res) => {
 
   res.sendStatus(200);
 });
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
